db828b8cc3b663eab6118dad1b5a4a31a0a985dd
Domt301 <leonardod.trimarchi@gmail.com>
2017-11-22 12:19:32 -0500

    inital commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Net.Http;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            TheProgram();
+        }
+
+
+        private static void TheProgram()
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+            string str;
+            int rCnt;
+            int cCnt;
+            int rw = 0;
+            int cl = 0;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(@"c:\11_22_IR.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;
+            cl = range.Columns.Count;
+
+            Console.WriteLine(rw);
+
+            for (rCnt = 1; rCnt <= rw; rCnt++)
+            {
+                for (cCnt = 1; cCnt <= cl; cCnt++)
+                {
+                    str = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+                    var result = str.Substring(str.LastIndexOf(':') + 1);
+                    Console.WriteLine(result);
+
+                    SendDataToServices(result);
+
+
+                }
+            }
+            Console.ReadLine();
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        private static void SendDataToServices(string stringer)
+        {
+            HttpClient client = new HttpClient();
+            var content2 = new StringContent(stringer.Trim());
+            var response =  client.PostAsync("services/api/address", content2);
+            var responseString = response.Result.ReasonPhrase.ToString();
+            Console.WriteLine(responseString);
+        }
+    }
+}
